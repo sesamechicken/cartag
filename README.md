93e212d46afd463a2d14098dfc90a3628ae0f167
Chris Dickow <chrisdickow@gmail.com>
2014-03-21 01:59:09 -0400

    Init commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 cartag
 ======
+Proof for cartag app
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cartag::Application.config.secret_token = '629d250bf3529dc813a33028f0fe4c4d833b4645186356fd253f18c4b1bc63c9dad10a5a804cc09775e492e1e02cd403579ec4761b6a30b1026e1b2c49919bec'
